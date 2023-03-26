c7c3c99bc474e46a6460443f22f9628a2ee55a5e
Ayush Yadav <110627770+ayush6790@users.noreply.github.com>
2023-03-27 02:05:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function clearScreen() {
+    document.getElementById("input").value = "";
+}
+ 
+function display(value) {
+    document.getElementById("input").value += value;
+}
+ 
+function calculate() {
+    var p = document.getElementById("input").value;
+    var q = eval(p);
+    document.getElementById("input").value = q;
+}
